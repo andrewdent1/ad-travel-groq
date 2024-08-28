e2b2f0f1d17791bad1f097760ff03d9d1af20140
Andrew Dent <155765937+andrewdent1@users.noreply.github.com>
2024-08-28 15:53:13 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript code to add interactivity to the website
+// For this example, we'll simply add a welcome message when the page loads
+window.addEventListener("load", () => {
+  document.getElementById("welcome").innerHTML = "Welcome to our travel website!";
+});
